6b36b673ee7cb53ba0c59da823a02cfc63575837
Wesley <wesley.rl@tuta.io>
2024-08-21 22:28:38 -0300

    (Improvement) Add config to restframework show the render API

--- openai_service/settings/production.py
+++ openai_service/settings/production.py
@@ -6,7 +6,6 @@ import mimetypes
 
 mimetypes.add_type("text/javascript", ".js", True)
 
-
 DEBUG = config("DEBUG", default=False, cast=bool)
 
 SECRET_KEY = config("SECRET_KEY")
@@ -50,6 +49,19 @@ STORAGES = {
     },
 }
 
-
-
-
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+}
